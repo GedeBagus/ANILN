0d20afeeadb44e319a9c0ba78b47b5000889ab78
Gede Bagus <bdarmagita@gmail.com>
2021-05-23 22:27:05 +0700

    Add new data

--- home.php
+++ home.php
@@ -32,7 +32,7 @@
   if (isset($_POST['search-aniln'])) {
     $test = $_POST['search-aniln'];
     $data = sparql_get(
-      "http://a2e4f77090d3.ngrok.io/aniln",
+      "http://63ed0738ab2f.ngrok.io/aniln",
       "
         PREFIX p: <http://Aniln.com>
         PREFIX d: <http://Aniln.com/ns/data#>
@@ -54,7 +54,7 @@
     );
   } else {
     $data = sparql_get(
-      "http://a2e4f77090d3.ngrok.io/aniln",
+      "http://63ed0738ab2f.ngrok.io/aniln",
       "
         PREFIX p: <http://Aniln.com>
         PREFIX d: <http://Aniln.com/ns/data#>
