afb2cb90c16a53a749473b90004ef72baef6886f
Gede Bagus <bdarmagita@gmail.com>
2021-05-23 16:25:56 +0700

    change .html

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
         </button>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="aboutus.php">About Us</a>
+            <a class="nav-link" href="aboutus.html">About Us</a>
           </li>
         </ul>
       </nav>
