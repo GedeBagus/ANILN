862f8bc9a6684a8c4bc03a7c2f6d653c7cb3ada4
Gede Bagus <bdarmagita@gmail.com>
2021-05-30 21:34:14 +0700

    update search + cleaing

--- home.php
+++ home.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="text/css" href=css/style.css>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -46,9 +45,9 @@
                 d:airedPublished ?Rilis;
                 d:studiosAuthor ?Penerbit;
                 d:status ?Status;
+                d:genre ?Genre;
                 d:episodeChapter ?JumlahEps
-                FILTER regex(?Nama, '$test')
-    
+                FILTER ((regex(?Nama, '$test') || regex(?Tipe, '$test') || regex(?Status,  '$test') || regex(?Genre,  '$test'))
         }
             "
     );
@@ -68,8 +67,8 @@
                 d:airedPublished ?Rilis;
                 d:studiosAuthor ?Penerbit;
                 d:status ?Status;
+                d:genre ?Genre;
                 d:episodeChapter ?JumlahEps
-    
         }
             "
     );
@@ -176,11 +175,6 @@
   </div>
   <footer>&copy; ANILN Otaku</footer>
 </body>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
 <style>
   body {
     font-family: 'Poppins', sans-serif;
